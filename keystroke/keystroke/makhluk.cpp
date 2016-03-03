ed9ea734787a75d7caeba842b84897fd9a71ccec
albertusk95 <albertusk15@gmail.com>
2016-03-03 23:46:10 +0700

    updated makhluk.cpp

--- keystroke/keystroke/makhluk.cpp
+++ keystroke/keystroke/makhluk.cpp
@@ -1,25 +1,53 @@
+//
+// File: makhluk.cpp
+//
+// IMPLEMENTASI abstract class makhluk.h
+//
+
 #include "makhluk.h"
 
 makhluk::makhluk() {
+	// konstruktor
+	// inisialisasi nilai koordinat awal objek (x, y) dengan 
+	// nilai random 
 	int x = rand() % 100;
-	int y = rand() % 100;
+	int y = rand() % 100; 
 	P.set(x, y);
-	//arah
+	
+	// inisialisasi nilai arah gerak awal objek dengan nilai random (0 - 7)
+	int arah = rand() % 8;
 }
 
 void makhluk::bergerak() {
-	if (arah = 'n')
-		P.move(0, -dt);
-	//teruskan untuk arah lain
+	// memindahkan objek ke posisi lain di bidang 
+	
+	/*
+	ketentuan: 
+	0 : timur
+	1 : tenggara
+	2 : seltan
+	3 : barat daya
+	4 : barat 
+	5 : barat laut
+	6 : utara
+	7 : timur laut
+	*/
+	
+	switch (arah) {
+		case 0 : P.move(power,0) ; break;
+		case 1 : P.move(power,-power) ; break;
+		case 2 : P.move(0,-power) ; break;
+		case 3 : P.move(-power,-power) ; break;
+		case 4 : P.move(-power,0) ; break;
+		case 5 : P.move(-power,power) ; break;
+		case 6 : P.move(0,power) ; break;
+		case 7 : P.move(power,power) ; break;
+	}
+		
 	//jika keluar dari bidang, bagaimana?
 }
 
-void makhluk::printlok() {
-	printf("lokasi = (%d,%d)\n", P.getX(), P.getY());
-}
-
-Point makhluk::getlok() { return P; };
-
-void makhluk::printstatmakhluk() {
-	makhluk::printlok();
+Point makhluk::getlok() { 
+	// mengembalikan posisi (x, y) objek di bidang 
+	return P; 
 }
