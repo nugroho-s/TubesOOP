518dfbd877ed9e7b8f2c1dbd4f28ee7d54b2a319
albertusk95 <albertusk15@gmail.com>
2016-03-04 10:24:04 +0700

    added herbivora.h

---
+++ keystroke/keystroke/herbivora.h
@@ -0,0 +1,35 @@
+#include "makhluk.h"
+
+// file: herbivora.h 
+// class herbivora mewarisi sifat dari makhluk 
+// method yang dimiliki sendiri (tidak diturunkan dari makhluk) :
+//		- herbivora()
+//		- virtual ~herbivora()
+//		- virtual void sembunyi()
+// data yang dimiliki sendiri (tidak diturunkan dari makhluk) :
+//		- int mlapar 
+
+
+class herbivora: public makhluk {
+public:
+	// konstruktor
+	herbivora();
+	
+	// destruktor
+	virtual ~herbivora();
+	
+	// memakan objek lain - kelas turunannya harus mengimplementasikan ini 
+	virtual void makan()=0;
+	
+	// bersembunyi di benteng kecil ATAU di cangkang utk kasus kura2
+	virtual void sembunyi();
+	
+	// objek turunan herbivora berpindah ke koordinat lain 
+	virtual void bergerak();
+	
+	// mendapatkan level kelaparan sebuah objek turunan herbivora  
+	virtual int getlapar();
+	
+protected:
+	int mlapar;		// level kelaparan sebuah objek turunan herbivora 
+};
