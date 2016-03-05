6aa56b55fb5e99505751ae343f7a199e3664509d
albertusk95 <albertusk15@gmail.com>
2016-03-06 01:03:35 +0700

    updated singa

--- keystroke/keystroke/singa.cpp
+++ keystroke/keystroke/singa.cpp
@@ -1,20 +1,46 @@
 #include "singa.h"
 
 singa::singa() {
+	// constructor 
 	mlapar = maxlapar;
 	power = 3;
 	dt = 2;
-	arah = 'n';
+	arah = 0;
+	umur = 30;
 }
 
-void singa::makan() {
-	mlapar = maxlapar;
+singa::singa(singa& s) {
+	// copy constructor
+	mlapar = s.mlapar;
+	power = s.power;
+	dt = s.dt;
+	arah = s.arah;
+	umur = s.umur;
+	P.set(s.getlok().getX(), s.getlok().getY());
 }
 
 singa& singa::operator= (singa& s) {
-	int x = rand() % 100;
-	int y = rand() % 100;
-	P.set(x, y);
-	mlapar = s.mlapar;
-	return *this;
+	// operator=
+	if (this == &s) { // jika melakukan assignment trhdp diri sendiri
+		return *this; 
+	} else { // jika bukan
+		mlapar = s.mlapar;
+		power = s.power;
+		dt = s.dt;
+		arah = s.arah;
+		umur = s.umur;
+		P.set(s.getlok().getX(), s.getlok().getY());
+		return *this;
+	}
 }
+
+singa::~singa() {
+	// destruktor
+	cout << "destructor singa" << endl;
+}
+
+void singa::makan() {
+	// jika memakan objek lain, maka level kelaparannya menjadi sangat kenyang (30)
+	mlapar = maxlapar;
+}
+
